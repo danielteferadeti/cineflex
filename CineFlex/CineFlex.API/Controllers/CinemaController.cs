71449db41735fc1ba154e6c492ec1c9563aa287e
TigistShiferaw <tgg909441@gmail.com>
2023-05-21 10:59:13 +0300

    Add controller for queries

---
+++ CineFlex/CineFlex.API/Controllers/CinemaController.cs
@@ -0,0 +1,35 @@
+﻿using BlogApp.Application.Responses;
+using CineFlex.Application.Features.Cinema.CQRS.Queries;
+using CineFlex.Application.Features.Cinema.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CineFlex.API.Controllers
+{
+    public class CinemaController:ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CinemaController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<CinemaDto>>> Get()
+        {
+            var Cinemas = await _mediator.Send(new GetCinemaListQuery());
+            return Ok(Cinemas);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CinemaDto>> Get(int id)
+        {
+            var Cinema = await _mediator.Send(new GetCinemaQuery { Id = id });
+            return Ok(Cinema);
+        }
+
+      
+    }
+}
+
